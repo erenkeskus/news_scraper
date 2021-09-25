43a402d9737a0b82390d3a417851fa5fe6b0fa6d
Eren Keşküş <eren01@gmail.com>
2021-09-25 22:02:02 +0300

    added a unique constraint to article, made columns non nullable

--- news_scraper/data_access/models.py
+++ news_scraper/data_access/models.py
@@ -1,21 +1,22 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Table, Integer, Column, TEXT, DateTime, ForeignKey
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 metadata = Base.metadata
 
 class Article(Base): 
     __tablename__ = 'article'
+    __tableargs__ = (UniqueConstraint('title', 'sub_title', 'text', name='uix_article'),)
 
     id = Column(Integer, primary_key=True)
-    title = Column(TEXT(convert_unicode=True))
-    sub_title = Column(TEXT(convert_unicode=True))
-    text = Column(TEXT(convert_unicode=True))
+    title = Column(TEXT(convert_unicode=True), nullable=False)
+    sub_title = Column(TEXT(convert_unicode=True), nullable=False)
+    text = Column(TEXT(convert_unicode=True), nullable=False)
     insert_datetime = Column(DateTime(), server_default=func.now())
     def __repr__(self): 
-        return f'Article(title={self.title}, \nsub_title={self.sub_title} \
-            \ntext={self.text[:50]}'
+        return f'Article(title={self.title}, sub_title={self.sub_title} text={self.text[:50]}'
 
     def __eq__(self, other): 
         if isinstance(other, Article): 
@@ -28,7 +29,10 @@ class ArticleOccurenceLog(Base):
     __tablename__ = 'article_occurence_log'
 
     id = Column(Integer, primary_key=True)
-    article_id = Column(ForeignKey('article.id'))
-    datetime = Column(DateTime(), server_default=func.now())
+    article_id = Column(ForeignKey('article.id'), nullable=False)
+    insert_datetime = Column(DateTime(), server_default=func.now(), nullable=False)
 
-
+    article = relationship(Article)
+    
+    def __repr__(self): 
+        return f'Article(title={self.article.title}, insert_datetime={self.insert_datetime}\n'
